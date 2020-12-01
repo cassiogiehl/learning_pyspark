dcdc930f1b914a4a3efe1def45d9978e9fb2bbcc
Cássio Giehl da Rosa <cassio.giehl@gmail.com>
2020-12-01 20:16:40 -0300

    error: Vector are in unnamed module of loader app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
 from transformations import Transformations
 from file import File
-from model import Model
+from pyspark.ml.feature import StandardScaler
+from pyspark.ml.regression import LinearRegression
 
 spark = SparkSession.builder \
                     .master('local') \
@@ -12,6 +13,7 @@ sc = spark.sparkContext
 
 df = File(spark).load_file(str("CaliforniaHousing/cal_housing.data"))
 
+# Transformação e padronização do dataframe
 transformation = Transformations(spark)
 df = df.transform(transformation.normalize_medianHouseValue) \
         .transform(transformation.add_roomsPerHousehold) \
@@ -20,8 +22,17 @@ df = df.transform(transformation.normalize_medianHouseValue) \
         .transform(transformation.reorder_columns) \
         .transform(transformation.standarization)
 
-model = Model(spark, df).fit_transform()
+# Pré processamento de dados
+standardScaler = StandardScaler(inputCol="features", outputCol="features_scaled")
+scaler = standardScaler.fit(df)
+scaled_df = scaler.transform(df)
+train_data, test_data = scaled_df.randomSplit([.8,.2],seed=1234)
 
-train_data, test_data = model.train_test_split()
+# Treinamento e predição
+reg = LinearRegression(labelCol='label', maxIter=10, regParam=0.3, elasticNetParam=0.8)
+model = reg.fit(train_data)
+predicted = model.predict(test_data)
 
-model.Regression(train_data)
+# Validação
+print(model.summary.rootMeanSquaredError)
+print(model.summary.r2)
